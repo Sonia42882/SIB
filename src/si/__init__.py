65d190af14942fd02309a6da340061dc4d5c8526
sonia42882 <pg42882@uminho.pt>
2022-09-26 11:46:08 +0100

    Authorship

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,6 +1,6 @@
-__author__ = "Fernando Cruz"
+__author__ = "Sónia Carvalho"
 __credits__ = ["Vítor Pereira, Fernando Cruz"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Fernando Cruz"
-__email__ = "fernando.cruz@ceb.uminho.pt"
+__maintainer__ = "Sónia Carvalho"
+__email__ = "pg42882@alunos.uminho.pt"
