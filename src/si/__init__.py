7de062fb1d6097648bd5464b8aa4032a096dfe8c
sonia42882 <pg42882@uminho.pt>
2022-10-17 11:48:55 +0100

    credits

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Sónia Carvalho"
-__credits__ = ["Vítor Pereira, Fernando Cruz"]
+__credits__ = ["Sónia Carvalho"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "Sónia Carvalho"
